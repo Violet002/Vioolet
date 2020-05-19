d3ee2f939b3c675e45a84872c4651d3743ccffe6
Violet002 <Vioolet02@gmail.com>
2020-05-19 15:28:57 +0800

    add autotest code

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,2 @@
+# coding = utf-8
+EXCEL_DIR = r"C:\Users\Hubert\Desktop\移动测试\data\test.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,5 @@
+
+BASE_URL="http://httpbin.org/"
+IP_URL = "/ip"
+LOCAL_IP = "182.139.27.96"
+POST_URL = "/post"
